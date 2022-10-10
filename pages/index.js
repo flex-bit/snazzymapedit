01593331d403775ef4a29b5404dc1ea46ea195cc
John Infante <johninfante@Johns-MBP.attlocal.net>
2022-10-09 20:57:27 -0400

    v2 commit 2nd

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { userAgent } from "next/server";
 import { useState } from "react";
 import styles from "../styles/Home.module.css";
-import EditSection from "../comps/editSection";
+import EditSection from "../comps/EditSection";
 import MapsComponent from "../comps/MapsComponent";
 
 export default function Home() {
