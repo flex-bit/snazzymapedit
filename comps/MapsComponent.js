5cc837b32e4da38c993d3076b66fd7bcae5fa3e5
John Infante <johninfante@Johns-MBP.attlocal.net>
2022-10-11 20:52:13 -0400

    loading on fetching api for maps set to know when is loading

--- comps/MapsComponent.js
+++ comps/MapsComponent.js
@@ -5,12 +5,14 @@ import loadingIcon from "./icons/loadingIcon";
 const MapsComponent = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [isPaste, setIsPaste] = useState(false);
+  const [isAPILoading, setIsAPILoading] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [isFinished, setIsFinished] = useState(false);
   const [maps, setMaps] = useState([]);
   const fetchData = () => {
     fetch("https://snazzymap-api.herokuapp.com/maps")
       .then((response) => {
+        setIsAPILoading(false);
         return response.json();
       })
       .then((data) => {
@@ -208,6 +210,140 @@ const MapsComponent = () => {
             ></path>
           </svg>
         </div>
+      </div>
+      <div className={`${styles.icon} ${!isAPILoading ? styles.hide : ""}`}>
+        <svg width="100%" height="100%" viewBox="0 0 24 24">
+          <circle cx="12" cy="2" r="0" fill="currentColor">
+            <animate
+              attributeName="r"
+              begin="0"
+              calcMode="spline"
+              dur="1s"
+              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+              repeatCount="indefinite"
+              values="0;2;0;0"
+            ></animate>
+          </circle>
+          <circle
+            cx="12"
+            cy="2"
+            r="0"
+            fill="currentColor"
+            transform="rotate(45 12 12)"
+          >
+            <animate
+              attributeName="r"
+              begin="0.125s"
+              calcMode="spline"
+              dur="1s"
+              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+              repeatCount="indefinite"
+              values="0;2;0;0"
+            ></animate>
+          </circle>
+          <circle
+            cx="12"
+            cy="2"
+            r="0"
+            fill="currentColor"
+            transform="rotate(90 12 12)"
+          >
+            <animate
+              attributeName="r"
+              begin="0.25s"
+              calcMode="spline"
+              dur="1s"
+              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+              repeatCount="indefinite"
+              values="0;2;0;0"
+            ></animate>
+          </circle>
+          <circle
+            cx="12"
+            cy="2"
+            r="0"
+            fill="currentColor"
+            transform="rotate(135 12 12)"
+          >
+            <animate
+              attributeName="r"
+              begin="0.375s"
+              calcMode="spline"
+              dur="1s"
+              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+              repeatCount="indefinite"
+              values="0;2;0;0"
+            ></animate>
+          </circle>
+          <circle
+            cx="12"
+            cy="2"
+            r="0"
+            fill="currentColor"
+            transform="rotate(180 12 12)"
+          >
+            <animate
+              attributeName="r"
+              begin="0.5s"
+              calcMode="spline"
+              dur="1s"
+              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+              repeatCount="indefinite"
+              values="0;2;0;0"
+            ></animate>
+          </circle>
+          <circle
+            cx="12"
+            cy="2"
+            r="0"
+            fill="currentColor"
+            transform="rotate(225 12 12)"
+          >
+            <animate
+              attributeName="r"
+              begin="0.625s"
+              calcMode="spline"
+              dur="1s"
+              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+              repeatCount="indefinite"
+              values="0;2;0;0"
+            ></animate>
+          </circle>
+          <circle
+            cx="12"
+            cy="2"
+            r="0"
+            fill="currentColor"
+            transform="rotate(270 12 12)"
+          >
+            <animate
+              attributeName="r"
+              begin="0.75s"
+              calcMode="spline"
+              dur="1s"
+              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+              repeatCount="indefinite"
+              values="0;2;0;0"
+            ></animate>
+          </circle>
+          <circle
+            cx="12"
+            cy="2"
+            r="0"
+            fill="currentColor"
+            transform="rotate(315 12 12)"
+          >
+            <animate
+              attributeName="r"
+              begin="0.875s"
+              calcMode="spline"
+              dur="1s"
+              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+              repeatCount="indefinite"
+              values="0;2;0;0"
+            ></animate>
+          </circle>
+        </svg>
       </div>
       <div className={`${styles.mapCards}`}>
         {maps
